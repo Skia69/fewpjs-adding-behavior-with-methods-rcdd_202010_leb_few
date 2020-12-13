ee72551cb69cf02258c2863f629cc787dd55e75e
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-13 19:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ class Bird {
   }
 
   speak() {
-    this.sex === "male" ? `It's me! ${this.name}, the parrot!`: `${this.name} says squawk!`
+    this.sex === "male" ? `It's me! ${this.name}, the parrot!`:`${this.name} says squawk!`
   }
 }
